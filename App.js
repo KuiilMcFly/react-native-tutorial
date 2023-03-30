5eae09da0938769688b2ae3ae5c76917a672e982
Carmine Esposito <105643457+KuiilMcFly@users.noreply.github.com>
2023-03-30 18:17:23 +0200

    todo complete

--- App.js
+++ App.js
@@ -1,26 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import React from 'react';
 
 
-export default function App() {
+export default class App extends React.Component {
+  state = {
+    value: '',
+    todoList: [],
+
+  }
+
+  addTodoHandler = () => {
+    if(this.state.value.trim() === ""){
+      alert('Scrivi qualcosa!')
+    }
+    this.setState(prevState => {
+      return {
+        todoList: prevState.todoList.concat(prevState.value),
+        value: ''
+      }
+    })
+    
+  }
+
+  onChangeTextHandler = (text) => {
+    this.setState({value: text})
+  }
+  render () {
+    
+    const list = this.state.todoList.filter(todo => todo.trim() !== "").map((todo, index) => {
+      return  <Text style={styles.Todo} key={index}>- {todo.trim()}</Text>
+    })
+
   return (
     <View style={styles.container}>
-      <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
-        <TextInput placeholder='Scrivi qualcosa' style={{borderBottomWidth: 2, marginBottom: 30,}}/>
-        <Button title='invio' onPress={() => {}}/>
+      <View style={styles.InputContainer}>
+        <TextInput value={this.state.value} onChangeText={this.onChangeTextHandler} placeholder='Scrivi qualcosa' style={styles.Input}/>
+        <Button title='invio' onPress={this.addTodoHandler}/>
+      </View>
+    <ScrollView>
+      <View style={styles.Output}>
+        {list}
       </View>
 
-      <View>
-
-      <Text>Ciao sto lavorando su react native</Text>
-      </View>
+    </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
   container: {
     padding: 50,
+  },
+
+  Input: {
+    width: '80%',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 4,
+  },
+
+  InputContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-around', 
+    alignItems: 'center',
+  },
+
+  Output: {
+    marginTop: 20,
+    
+  },
+
+  Todo: {
+    fontSize:17,
+    borderWidth: 1,
+    padding: 5,
+    borderRadius:5,
+    backgroundColor: 'gray',
+    margin: 10,
   },
 });
